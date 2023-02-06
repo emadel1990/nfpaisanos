e8e90eb34c024c5398007cbc20fd6ba6922adeb9
Jose Emanuel Delgado <emadevjs@gmail.com>
2023-02-06 09:23:37 -0300

    avances slider

--- src/components/filters/filterPrice/FilterPrice.tsx
+++ src/components/filters/filterPrice/FilterPrice.tsx
@@ -1,20 +1,31 @@
 'use client';
 
 import style from './filterPrice.module.css';
-import { Slider } from '@mui/material';
+
+import { Poppins } from '@next/font/google';
+import { Slider, Typography, Stack } from '@mui/material';
+import { CustomSlider } from '@/src/components/globals/customSlider';
+
+const poppins = Poppins({
+	weight: '700',
+	style: 'normal',
+	subsets: ['latin']
+});
 
 export const FilterPrice = () => {
 	return (
-		<div>
-			<Slider
-				sx={{
-					'& .MuiSlider-rail': {
-						backgroundColor: 'red !important',
-						height: '20px !important',
-						borderRadius: '10px 20px 20px 10px'
-					}
-				}}
-			/>
-		</div>
+		<Stack
+			flex={1}
+			rowGap={1}>
+			<Typography className={`${poppins.className} ${style.priceRange}`}>PRICE RANGE</Typography>
+			<CustomSlider />
+			<Stack
+				flex={1}
+				flexDirection={'row'}
+				justifyContent={'space-between'}>
+				<Typography className={`${poppins.className} ${style.priceEth}`}>0.01 ETH</Typography>
+				<Typography className={`${poppins.className} ${style.priceEth}`}>10 ETH</Typography>
+			</Stack>
+		</Stack>
 	);
 };
